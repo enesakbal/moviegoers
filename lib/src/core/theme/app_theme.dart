db91851334795ddc8eecba0d8b7192316a4ba540
Enes Akbal <enes.akbal@fcbhealthreaktor.com>
2023-03-25 03:49:14 +0300

    feat: added theme

---
+++ lib/src/core/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:moviegoers/src/config/gen/colors.gen.dart';
+
+class MGAppTheme {
+  ThemeData get theme => ThemeData(
+        appBarTheme: _appbarTheme(),
+        scaffoldBackgroundColor: MGColors.dark,
+      );
+
+  AppBarTheme _appbarTheme() {
+    return AppBarTheme(
+      backgroundColor: Colors.white,
+      centerTitle: false,
+      elevation: 0,
+      titleTextStyle: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 16.sp,
+      ),
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+        statusBarColor: Colors.transparent,
+      ),
+    );
+  }
+}
