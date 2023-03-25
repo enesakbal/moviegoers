7cf8152db7500f6fd3ef6a77260f5ebb5917e4c8
Enes Akbal <enes.akbal@fcbhealthreaktor.com>
2023-03-25 03:49:03 +0300

    feat: added auto route

---
+++ lib/src/config/router/app_router.dart
@@ -0,0 +1,41 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+import 'package:moviegoers/src/presentation/view/splash_view.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'View,Route')
+class AppRouter extends _$AppRouter {
+  @override
+  final List<AutoRoute> routes = [
+    RedirectRoute(path: '/', redirectTo: '/splash'),
+    AutoRoute(page: SplashRoute.page, path: '/splash'),
+  ];
+}
+
+final router = AppRouter();
+
+
+// Widget zoomInTransition(BuildContext context, Animation<double> animation,
+//     Animation<double> secondaryAnimation, Widget child) {
+//   final opacity = Tween<double>(
+//     end: 1,
+//     begin: 0,
+//   ).chain(CurveTween(curve: Curves.fastOutSlowIn)).animate(animation);
+
+//   final scale = Tween<double>(
+//     end: 1,
+//     begin: 1.20,
+//   ).chain(CurveTween(curve: Curves.fastOutSlowIn)).animate(animation);
+
+//   return FadeTransition(
+//     opacity: opacity,
+//     child: ScaleTransition(
+//       scale: scale,
+//       child: child,
+//     ),
+//   );
+// }
+
+// final router = AppRouter();
---
+++ lib/src/config/router/navigator_observers.dart
@@ -0,0 +1,23 @@
+import 'dart:developer';
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+class NavigatorObservers extends AutoRouterObserver {
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    log('New route pushed: ${route.settings.name}');
+  }
+
+  @override
+  void didInitTabRoute(TabPageRoute route, TabPageRoute? previousRoute) {
+    log('Tab route visited: ${route.name}');
+  }
+
+  @override
+  void didChangeTabRoute(TabPageRoute route, TabPageRoute previousRoute) {
+    log('Tab route re-visited: ${route.name}');
+  }
+}
+
+final observer = NavigatorObservers();
