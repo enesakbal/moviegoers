4d7b7563aa33fd2c61e5d9e78e854c7e05c3c2f4
Enes Akbal <enes.akbal@fcbhealthreaktor.com>
2023-03-25 03:46:01 +0300

    feat: generated colors from xml

---
+++ lib/src/config/gen/colors.gen.dart
@@ -0,0 +1,96 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class MGColors {
+  MGColors._();
+
+  /// MaterialColor:
+  ///   50: #FFE7EBFF
+  ///   100: #FFC2CCFF
+  ///   200: #FF9AABFF
+  ///   300: #FF7189FF
+  ///   400: #FF526FFF
+  ///   500: #FF3456FF
+  ///   600: #FF2F4FFF
+  ///   700: #FF2745FF
+  ///   800: #FF213CFF
+  ///   900: #FF152BFF
+  static const MaterialColor blue = MaterialColor(
+    0xFF3456FF,
+    <int, Color>{
+      50: Color(0xFFE7EBFF),
+      100: Color(0xFFC2CCFF),
+      200: Color(0xFF9AABFF),
+      300: Color(0xFF7189FF),
+      400: Color(0xFF526FFF),
+      500: Color(0xFF3456FF),
+      600: Color(0xFF2F4FFF),
+      700: Color(0xFF2745FF),
+      800: Color(0xFF213CFF),
+      900: Color(0xFF152BFF),
+    },
+  );
+
+  /// MaterialColor:
+  ///   50: #FFE2E2E3
+  ///   100: #FFB6B8B9
+  ///   200: #FF86888B
+  ///   300: #FF55585D
+  ///   400: #FF30353A
+  ///   500: #FF0C1117
+  ///   600: #FF0A0F14
+  ///   700: #FF080C11
+  ///   800: #FF060A0D
+  ///   900: #FF030507
+  static const MaterialColor dark = MaterialColor(
+    0xFF0C1117,
+    <int, Color>{
+      50: Color(0xFFE2E2E3),
+      100: Color(0xFFB6B8B9),
+      200: Color(0xFF86888B),
+      300: Color(0xFF55585D),
+      400: Color(0xFF30353A),
+      500: Color(0xFF0C1117),
+      600: Color(0xFF0A0F14),
+      700: Color(0xFF080C11),
+      800: Color(0xFF060A0D),
+      900: Color(0xFF030507),
+    },
+  );
+
+  /// MaterialColor:
+  ///   50: #FFE2F4E5
+  ///   100: #FFB7E3BE
+  ///   200: #FF87D193
+  ///   300: #FF57BE67
+  ///   400: #FF33B047
+  ///   500: #FF0FA226
+  ///   600: #FF0D9A22
+  ///   700: #FF0B901C
+  ///   800: #FF088617
+  ///   900: #FF04750D
+  static const MaterialColor green = MaterialColor(
+    0xFF0FA226,
+    <int, Color>{
+      50: Color(0xFFE2F4E5),
+      100: Color(0xFFB7E3BE),
+      200: Color(0xFF87D193),
+      300: Color(0xFF57BE67),
+      400: Color(0xFF33B047),
+      500: Color(0xFF0FA226),
+      600: Color(0xFF0D9A22),
+      700: Color(0xFF0B901C),
+      800: Color(0xFF088617),
+      900: Color(0xFF04750D),
+    },
+  );
+}
