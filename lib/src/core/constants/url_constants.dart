c82e11d1f8da5d4f82cf2660e45918bdb467ea3d
Enes Akbal <enes.akbal@fcbhealthreaktor.com>
2023-03-26 05:58:42 +0300

    feat: added url constants

---
+++ lib/src/core/constants/url_constants.dart
@@ -0,0 +1,17 @@
+class UrlContants {
+  //* movie lists
+  static const popularMovies = '/movies/popular';
+  static const nowPlayingMovies = '/movie/now_playing';
+  static const upcomingMovies = '/movies/upcoming';
+
+  //* movie info
+  static const movieDetail = '/movies/{movie_id}';
+  static const movieCredits = '/movie/{movie_id}/credits';
+  static const movieExternalIDs = '/movie/{movie_id}/external_ids';
+  static const movieVideos = '/movie/{movie_id}/videos';
+  static const movieRecommendations = '/movie/{movie_id}/recommendations';
+  static const movieSimilar = '/movie/{movie_id}/similar';
+
+  //* rate
+  static const movieRating = '/movie/{movie_id}/rating';
+}
