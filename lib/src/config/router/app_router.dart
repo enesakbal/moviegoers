2169aa62d52b61dacfd4a0427e31cd436ae60845
Enes Akbal <enes.akbal@fcbhealthreaktor.com>
2023-04-16 18:19:42 +0300

    feat: added movie detail view to router

--- lib/src/config/router/app_router.dart
+++ lib/src/config/router/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 
 import '../../presentation/view/_project_components_view.dart';
 import '../../presentation/view/home_view.dart';
+import '../../presentation/view/movie_detail_view.dart';
 import '../../presentation/view/splash_view.dart';
 
 part 'app_router.gr.dart';
@@ -12,9 +14,42 @@ class AppRouter extends _$AppRouter {
   final List<AutoRoute> routes = [
     RedirectRoute(path: '/', redirectTo: '/home'),
     AutoRoute(page: SplashRoute.page, path: '/splash'),
-    AutoRoute(page: HomeRoute.page, path: '/home'),
+    CustomRoute(
+      page: HomeRoute.page,
+      path: '/home',
+      durationInMilliseconds: 500,
+      transitionsBuilder: TransitionsBuilders.zoomIn,
+    ),
+    CustomRoute(
+      page: MovieDetailRoute.page,
+      path: '/movie_detail',
+      durationInMilliseconds: 500,
+      reverseDurationInMilliseconds: 500,
+      transitionsBuilder: TransitionsBuilders.fadeIn,
+    ),
     AutoRoute(page: ProjectComponentsRoute.page, path: '/components'),
   ];
+}
+
+Widget zoomInTransition(
+    BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+  final opacity = Tween<double>(
+    end: 1,
+    begin: 0,
+  ).chain(CurveTween(curve: Curves.fastOutSlowIn)).animate(animation);
+
+  final scale = Tween<double>(
+    end: 1,
+    begin: 1.20,
+  ).chain(CurveTween(curve: Curves.fastOutSlowIn)).animate(animation);
+
+  return FadeTransition(
+    opacity: opacity,
+    child: ScaleTransition(
+      scale: scale,
+      child: child,
+    ),
+  );
 }
 
 final router = AppRouter();
--- lib/src/config/router/app_router.gr.dart
+++ lib/src/config/router/app_router.gr.dart
@@ -33,6 +33,16 @@ abstract class _$AppRouter extends RootStackRouter {
         child: const SplashView(),
       );
     },
+    MovieDetailRoute.name: (routeData) {
+      final args = routeData.argsAs<MovieDetailRouteArgs>();
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: MovieDetailView(
+          key: args.key,
+          movieID: args.movieID,
+        ),
+      );
+    },
   };
 }
 
@@ -76,4 +86,42 @@ class SplashRoute extends PageRouteInfo<void> {
   static const String name = 'SplashRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [MovieDetailView]
+class MovieDetailRoute extends PageRouteInfo<MovieDetailRouteArgs> {
+  MovieDetailRoute({
+    Key? key,
+    required String movieID,
+    List<PageRouteInfo>? children,
+  }) : super(
+          MovieDetailRoute.name,
+          args: MovieDetailRouteArgs(
+            key: key,
+            movieID: movieID,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'MovieDetailRoute';
+
+  static const PageInfo<MovieDetailRouteArgs> page =
+      PageInfo<MovieDetailRouteArgs>(name);
+}
+
+class MovieDetailRouteArgs {
+  const MovieDetailRouteArgs({
+    this.key,
+    required this.movieID,
+  });
+
+  final Key? key;
+
+  final String movieID;
+
+  @override
+  String toString() {
+    return 'MovieDetailRouteArgs{key: $key, movieID: $movieID}';
+  }
 }
