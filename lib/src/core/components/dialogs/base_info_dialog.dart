2278dea94684f740003c82d1a2af1b6d3b4e44b3
Enes Akbal <enesakbal00@gmail.com>
2023-05-16 16:17:46 +0300

    feat: added close button to base_info_dialog

--- lib/src/core/components/dialogs/base_info_dialog.dart
+++ lib/src/core/components/dialogs/base_info_dialog.dart
@@ -3,6 +3,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../config/gen/colors.gen.dart';
+import '../../../config/router/app_router.dart';
+import '../buttons/base_icon_button.dart';
 import 'dialog_interface.dart';
 
 class BaseInfoDialog extends IDialog {
@@ -27,16 +29,26 @@ class BaseInfoDialog extends IDialog {
     }, []);
 
     return SimpleDialog(
-      backgroundColor: MGColors.dark.shade400,
+      backgroundColor: MGColors.grey,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       insetPadding: const EdgeInsets.all(10),
       contentPadding: EdgeInsets.all(10.r),
-      titlePadding: const EdgeInsets.all(20),
-      title: Text(title, textAlign: TextAlign.center),
-      titleTextStyle: Theme.of(context).textTheme.titleMedium,
+      titlePadding: EdgeInsets.fromLTRB(20.r, 20.r, 20.r, 10.r),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title, textAlign: TextAlign.center),
+          BaseIconButton(
+            icon: const Icon(Icons.close, color: Colors.white),
+            onPressed: router.pop,
+            outline: true,
+          ),
+        ],
+      ),
+      titleTextStyle: Theme.of(context).textTheme.titleMedium!.copyWith(color: MGColors.dark.shade50),
       children: [
         content,
-        40.verticalSpace,
+        20.verticalSpace,
       ],
     );
   }
