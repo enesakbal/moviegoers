206d30fe46422d03b9f0aa6e978d381e6e774eae
Enes Akbal <enesakbal00@gmail.com>
2023-05-17 18:48:31 +0300

    ref: extesion on MovieListType enum

--- lib/src/core/constants/enum/movie_enum.dart
+++ lib/src/core/constants/enum/movie_enum.dart
@@ -1,1 +1,14 @@
-enum MovieTypes { POPULAR, UPCOMING, NOWPLAYING }
+enum MovieListTypes { POPULAR, UPCOMING, NOWPLAYING }
+
+extension Extension on MovieListTypes {
+  String name() {
+    switch (this) {
+      case MovieListTypes.POPULAR:
+        return 'Popular';
+      case MovieListTypes.UPCOMING:
+        return 'Upcoming';
+      case MovieListTypes.NOWPLAYING:
+        return 'Now Playing';
+    }
+  }
+}
