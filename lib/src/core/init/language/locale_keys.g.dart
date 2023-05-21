2695d479bba3521ca6d0f90c4bd6ea2f1030542a
Enes Akbal <enesakbal00@gmail.com>
2023-05-21 21:26:21 +0300

    ref:

--- lib/src/core/init/language/locale_keys.g.dart
+++ lib/src/core/init/language/locale_keys.g.dart
@@ -1,11 +1,13 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-abstract class LocaleKeys {
+abstract class  LocaleKeys {
   static const splash_welcome = 'splash.welcome';
   static const splash = 'splash';
   static const home_popular = 'home.popular';
   static const home_upcoming = 'home.upcoming';
   static const home_now_playing = 'home.now_playing';
+  static const home_top_rated = 'home.top_rated';
   static const home_view_all = 'home.view_all';
   static const home = 'home';
+
 }
