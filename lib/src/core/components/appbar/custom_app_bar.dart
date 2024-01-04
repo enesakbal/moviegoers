a3ecfef21eb68a1b7cd927136cd8bb77ce9508c7
Enes Akbal <enesakbal00@gmail.com>
2024-01-04 19:44:30 +0300

    fix: fixed import issue

--- lib/src/core/components/appbar/custom_app_bar.dart
+++ lib/src/core/components/appbar/custom_app_bar.dart
@@ -1,5 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide SearchBar;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../config/gen/assets.gen.dart';
